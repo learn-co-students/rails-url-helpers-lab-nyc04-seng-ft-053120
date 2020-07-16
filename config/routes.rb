c3abcd0863c7b2cc86d0957d2dc973c2df112df6
tracycollins <tc@threeceemedia.com>
2020-07-16 17:26:45 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students do
+    member do
+      get 'activate'
+    end
+  end
+  # resource :activate_student
+  # get '/activate_student', to: 'students#activate'
+  # post '/activate_student/:id', to: 'students#activate_student'
+  # post '/students/:id/activate', to: 'students#activate' as 'activate_student_path'
+
 end
